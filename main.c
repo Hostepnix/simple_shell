8d56cfae633d3676f17078f595d45eb825b666a1
Hostepnix <stephane4love@gmail.com>
2022-08-10 13:18:02 -0700

    Update Simple Shell Project

--- main.c
+++ main.c
@@ -1,57 +1,14 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * main - Runs a simple UNIX command interpreter.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - Runs a simple UNIX command interpreter
+ * @argc: The number of command line arguments supplied to the program
+ * @argv: An array of pointers containing command line arguments
  *
- * Return: The return value of the last executed command.
+ * Return: success (0), fail (1)
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char __attribute__((unused)) **argv)
 {
-	int ret_val = 0, retn;
-	int *exe_ret = &retn;
-	char shell_prompt[15] = "haksam";
-	char *prompt = _strcat(shell_prompt, " $ "), *new_com = "\n";
-
-	name = argv[0];
-	hist = 1;
-	ll_aliases = NULL;
-	signal(SIGINT, sig_handler);
-
-	*exe_ret = 0;
-	environ = _copyenv();
-	if (!environ)
-		exit(-100);
-	if (argc != 1)
-	{
-		ret_val = file_stdin(argv[1], exe_ret);
-		free_env();
-		free_alias_list(ll_aliases);
-		return (*exe_ret);
-	}
-	if (!isatty(STDIN_FILENO))
-	{
-		while (ret_val != END_OF_FILE && ret_val != EXIT)
-			ret_val = handle_argv(exe_ret);
-		free_env();
-		free_alias_list(ll_aliases);
-		return (*exe_ret);
-	}
-	while (1)
-	{
-		write(STDOUT_FILENO, prompt, 15);
-		ret_val = handle_argv(exe_ret);
-		if (ret_val == END_OF_FILE || ret_val == EXIT)
-		{
-			if (ret_val == END_OF_FILE)
-				write(STDOUT_FILENO, new_com, 1);
-			free_env();
-			free_alias_list(ll_aliases);
-			exit(*exe_ret);
-		}
-	}
-	free_env();
-	free_alias_list(ll_aliases);
-	return (*exe_ret);
+	start_shell(); /*run shell loop until exit condition is met*/
+	exit(EXIT_SUCCESS);
 }
